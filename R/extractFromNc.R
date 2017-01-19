244705e61affa5c00e9eaa4d796e33184d97cc0d
Ross Holmberg <rholmberg@penguins.org.au>
2017-01-19 16:41:41 +1100

    adjusted to reduce memory usage

--- R/extractFromNc.R
+++ R/extractFromNc.R
@@ -16,7 +16,6 @@
 extractFromNc <- function( file, data.variable ) {
 
   con <- ncdf4::nc_open( filename = file )
-  data <- ncdf4::ncvar_get( nc = con, varid = data.variable )
 
   lonlist <- ncdf4::ncvar_get( nc = con, varid = "longitude" ) %>%
     as.vector( mode = "numeric" )
@@ -32,16 +31,11 @@ extractFromNc <- function( file, data.variable ) {
     as.POSIXct( origin = "1970-01-01", tz = "UTC" ) %>%
     as.Date()
 
-  ncdf4::nc_close( con )
-
-  datadt <- lapply( X = seq_along( data[1,1,] ),
+  output[ , as.character( times ) := lapply( X = seq_along( data[1,1,] ),
                     FUN = function( x, data ) { as.vector( data[,,x] ) },
-                    data = data ) %>%
-    do.call( what = cbind ) %>%
-    data.table::data.table() %>%
-    data.table::setnames( as.character( times ) )
+                    data = ncdf4::ncvar_get( nc = con, varid = data.variable ) ) ]
 
-  output <- cbind( output, datadt )
+  ncdf4::nc_close( con )
 
   return( output )
 
