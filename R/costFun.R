d0108c83b9ab302fcdfb4cbad75f6d4a3a1eaa86
Ross Holmberg <rholmberg@penguins.org.au>
2017-02-09 17:00:09 +1100

    SIGNIFICANT changes. allowing for better use of multi-dimensional arrays. Output is now in the form of a list.

--- R/costFun.R
+++ R/costFun.R
@@ -7,30 +7,47 @@
 #' @param angle.sink A matrix of angles calculated using angleSink.
 #' @keywords cost wind sink
 #' @importFrom circular rad
+#' @importFrom plyr laply
+#' @importFrom plyr alply
 #' @export
 #' @return A numeric array, with dimensions matching water.direction and water.speed
 
 
 costFun <- function( water.direction, water.speed, angle.sink ) {
 
-  # ensure all array dimensions match correctly before proceeding
-  stopifnot(
-    identical( dim( water.direction ), dim( water.speed ) ),
-    identical( dim( water.direction )[ 1:2 ], dim( angle.sink ) )
-  )
+    # ensure all array dimensions match correctly before proceeding
+    stopifnot(
+        identical( dim( water.direction ), dim( water.speed ) )
+    )
 
-  betta <- base::mapply(
-    FUN = function( x ) {
-      abs( circular::rad( water.direction[,,x] ) - circular::rad( angle.sink ) )
-    },
-    x = seq_along( water.direction[1,1,] ),
-    SIMPLIFY = T
-  )
+    # try to determine which dimensions represent the ones we're after.
+    if( identical( dim( water.direction )[ 1:2 ], dim( angle.sink ) ) ) {
+        dir.dims <- c( 1L, 2L )
+        seq.dim <- 3L
+    } else if( identical( dim( water.direction )[ 2:3 ], dim( angle.sink ) ) ) {
+        dir.dims <- c( 2L, 3L )
+        sequence <- seq_along( water.direction[,1,1] )
+        seq.dim <- 1L
+    } else if( identical( dim( water.direction )[ 3:1 ], dim( angle.sink ) ) ) {
+        dir.dims <- c( 3L, 1L )
+        sequence <- seq_along( water.direction[1,,1] )
+        seq.dim <- 2L
+    } else {
+        stop( "Could not find appropriate dimensions to use in currents array." )
+    }
 
-  betta <- array( betta, dim = dim( water.direction ) )
+    betta <- plyr::laply(
+        .data = plyr::alply( water.direction, .margins = seq.dim, as.array ),
+        .fun = function( x, angle.sink ) {
+            abs( circular::rad( x ) - circular::rad( angle.sink ) )
+        },
+        angle.sink = angle.sink
+    )
 
-  cost.sink <- ( 0.6184 * betta - 0.0984 * betta ^ 2 ) * water.speed
+    cost.sink <- ( 0.6184 * betta - 0.0984 * betta ^ 2 ) * water.speed
 
-  return( cost.sink )
+    cost.sink <- plyr::alply( .data = cost.sink, .margins = seq.dim, as.array )
+
+    return( cost.sink )
 
 }
